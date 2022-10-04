a968e9c8ed2ce73818354dba50eac3f9b5d31be0
Budhi <gurung.b1t@gmail.com>
2022-10-04 16:51:08 -0400

    initial commit

---
+++ src/main/java/com/example/jwt_basic/JwtBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt_basic/JwtBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
