7fbacd1c1d0cb4af2ab590d2231f01c5d46763c1
Budhi <gurung.b1t@gmail.com>
2022-10-05 15:54:58 -0400

    basic API key authentication

--- src/main/java/com/example/jwt_basic/Configuration/Security/Authentication/ApiKeyAuthentication.java
+++ src/main/java/com/example/jwt_basic/Configuration/Security/Authentication/ApiKeyAuthentication.java
@@ -2,54 +2,66 @@ package com.example.jwt_basic.Configuration.Security.Authentication;
 
 import java.util.Collection;
 
+import javax.security.auth.Subject;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
-@Setter
-@Getter
-public class CustomAuthentication implements Authentication {
+@RequiredArgsConstructor
+public class ApiKeyAuthentication implements Authentication{
 
-    private final Boolean authentication;
     private final String key;
+    private Boolean authenticated;
 
     @Override
     public String getName() {
+        // TODO Auto-generated method stub
         return null;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public boolean implies(Subject subject){
+        return Authentication.super.implies(subject);
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Object getCredentials() {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Object getDetails() {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Object getPrincipal() {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public boolean isAuthenticated() {
-        return authentication;
+       return authenticated;
     }
 
     @Override
     public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-        
+        this.authenticated = isAuthenticated;        
     }
     
 }
